fa98bfd82ddf8aa4d33ae6696f872cf02b6f61de
sbrockerhoff <162063986+sbrockerhoff@users.noreply.github.com>
2024-05-11 20:50:21 -0400

    Update tb_cpu.sv

--- catalog/cpu/tb_cpu.sv
+++ catalog/cpu/tb_cpu.sv
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // The Cooper Union
 // ECE 251 Spring 2024
-// Engineer: YOUR NAMES
+// Engineer: Stephen Brockerhoff & Abduhla Ali
 // 
 //     Create Date: 2023-02-07
 //     Module Name: tb_cpu
@@ -10,14 +10,87 @@
 // Revision: 1.0
 //
 //////////////////////////////////////////////////////////////////////////////////
-`ifndef TB_CPU
-`define TB_CPU
-
 `timescale 1ns/100ps
-`include "cpu.sv"
 
-module tb_cpu;
-    parameter n = 32;
+`include "./cpu.sv"
+
+module tb_cpu();
+    logic rst;
+    logic [15:0] instruction, readData;
+
+    logic memWrite;
+    logic [15:0] aluResult, memWriteData, pc;
+
+    reg clk, reset; //reset for initializing testvectors
+
+    logic [87:0] testvectors[0:1000];
+    logic [87:0] tmp;
+    integer vectornum, errors;
+
+    //expected
+    logic expectedMemWrite;
+    logic [15:0] expectedAluResult, expectedMemWriteData, expectedPc;
+
+    cpu uut (
+             .clk(clk),
+             .rst(rst),
+             .instruction(instruction),
+             .readData(readData),
+             .memWrite(memWrite),
+             .aluResult(aluResult),
+             .memWriteData(memWriteData),
+             .pc(pc)
+    );
+
+    always begin
+        clk = 1;
+        #5;
+        clk = 0;
+        #5;
+    end
+
+    initial begin
+        $dumpfile("tb_cpu_cpu.vcd");
+        $dumpvars(0, uut);
+    end
+
+    initial begin
+        $readmemh("tb_cpu_tb.tv", testvectors);
+        vectornum = 0;
+        errors = 0;
+        reset = 1; #27; reset = 0;
+    end
+
+    always @(posedge clk) begin
+        tmp = testvectors[vectornum];
+
+        rst = tmp[84];
+        instruction = tmp[83:68];
+        readData = tmp[67:52];
+        expectedMemWrite = tmp[51:48];
+        expectedAluResult = tmp[47:32];
+        expectedMemWriteData = tmp[31:16];
+        expectedPc = tmp[15:0];
+
+    end
+
+    always @(negedge clk) begin
+        if (~reset) begin
+            if ({memWrite, aluResult, memWriteData, pc} !== {expectedMemWrite, expectedAluResult, expectedMemWriteData, expectedPc}) begin
+                $display("Error:\tinputs: {rst, instruction, readData} = %b %b %b", rst, instruction, readData);
+                $display("\tmemWrite= %h, expectedMemWrite = %h", memWrite, expectedMemWrite);
+                $display("\taluResult = %h, expectedAluResult = %h", aluResult, expectedAluResult);
+                $display("\tmemWriteData = %h, expectedMemWriteData = %h", memWriteData, expectedMemWriteData);
+                $display("\tpc = %h, expectedPc = %h", pc, expectedPc);
+                errors = errors + 1;
+            end
+            vectornum = vectornum + 1;
+            if (testvectors[vectornum] === 88'hx) begin
+                $display("%d tests completed with %d errors", vectornum, errors);
+                $finish;
+            end
+        end
+    end
+
 
 endmodule
-`endif // TB_CPU
