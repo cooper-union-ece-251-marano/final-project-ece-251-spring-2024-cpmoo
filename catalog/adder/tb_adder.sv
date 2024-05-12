9435026357d68a41567fd24a9dfbea93b27599ed
sbrockerhoff <162063986+sbrockerhoff@users.noreply.github.com>
2024-05-11 21:05:13 -0400

    Update tb_adder.sv

--- catalog/adder/tb_adder.sv
+++ catalog/adder/tb_adder.sv
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // The Cooper Union
 // ECE 251 Spring 2024
-// Engineer: YOUR NAMES
+// Engineer: Stephen Brockerhoff & Abduhla Ali
 // 
 //     Create Date: 2023-02-07
 //     Module Name: tb_adder
@@ -10,29 +10,72 @@
 // Revision: 1.0
 //
 //////////////////////////////////////////////////////////////////////////////////
-`ifndef TB_ADDER
-`define TB_ADDER
-
 `timescale 1ns/100ps
-`include "adder.sv"
 
-module tb_adder;
-    parameter n = 32;
-    logic [(n-1):0] a, b, y;
+`include "./adder.sv"
 
-   initial begin
-        $dumpfile("adder.vcd");
-        $dumpvars(0, uut);
-        $monitor("a = 0x%0h b = 0x%0h y = 0x%0h", a, b, y);
+module adder_tb();
+    logic [15:0] a;
+    logic [15:0] b;
+    logic [15:0] result;
+
+    reg clk, reset; //reset for initializing testvectors
+
+    logic [47:0] testvectors[0:3];
+    logic [47:0] tmp;
+    integer vectornum, errors;
+    logic [15:0] expectedResult;
+
+
+    adder uut (
+             .a(a),
+             .b(b),
+             .result(result)
+    );
+
+    always begin
+        clk = 1;
+        #5;
+        clk = 0;
+        #5;
     end
 
     initial begin
-        a <= #n'hFFFFFFFF;
-        b <= #n'hFFFFFFFF;
+        $dumpfile("adder.vcd");
+        $dumpvars(0, uut);
     end
 
-    adder uut(
-        .A(a), .B(b), .Y(y)
-    );
+    initial begin
+        $readmemh("adder_tb.tv", testvectors);
+        vectornum = 0;
+        errors = 0;
+        reset = 1; #27; reset = 0;
+    end
+
+    always @(posedge clk) begin
+        #1; tmp = testvectors[vectornum];
+
+        a = tmp[47:32];
+        b = tmp[31:16];
+
+        expectedResult = tmp[15:0];
+    end
+
+    always @(negedge clk) begin
+        #1;
+        if (~reset) begin
+            if (result !== expectedResult) begin
+                $display("Error:\tinputs: a = %h, b=%h", a, b);
+                $display("\tresult = %h, expectedResult = %h", result, expectedResult);
+                errors = errors + 1;
+            end
+            vectornum = vectornum + 1;
+            if (testvectors[vectornum] === 48'hx) begin
+                $display("%d tests completed with %d errors", vectornum, errors);
+                $finish;
+            end
+        end
+    end
+
+
 endmodule
-`endif // TB_ADDER
