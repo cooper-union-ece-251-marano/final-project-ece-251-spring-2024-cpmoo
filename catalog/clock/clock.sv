4137c98ae91d33b567fe67ced11e6b9591c3e5f8
sbrockerhoff <162063986+sbrockerhoff@users.noreply.github.com>
2024-05-11 21:08:43 -0400

    Update clock.sv

--- catalog/clock/clock.sv
+++ catalog/clock/clock.sv
@@ -17,20 +17,15 @@
 
 module clock
     #(parameter ticks = 10)(
-    //
-    // ---------------- PORT DEFINITIONS ----------------
-    //
+ 
     input ENABLE,
     output reg CLOCK
 );
-    //
-    // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
-    //
+
     reg start_clock;
-    real clock_on = ticks/2; // duty cycle = 50%
+    real clock_on = ticks/2; 
     real clock_off = ticks/2;
 
-    // initialize variables
     initial begin
       CLOCK <= 0;
       start_clock <= 0;
@@ -43,7 +38,7 @@ module clock
         else begin
             start_clock = 0;
         end
-        // #ticks CLOCK = ~CLOCK;
+
     end
     always @(start_clock) begin
         CLOCK = 0;
@@ -55,4 +50,4 @@ module clock
     end
 endmodule
 
-`endif // CLOCK
+`endif 
