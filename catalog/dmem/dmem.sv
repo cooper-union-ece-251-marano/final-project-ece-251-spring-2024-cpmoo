52370868d8436dc73296bcf6c6bfcb6514b574b1
sbrockerhoff <162063986+sbrockerhoff@users.noreply.github.com>
2024-05-11 21:09:51 -0400

    Update dmem.sv

--- catalog/dmem/dmem.sv
+++ catalog/dmem/dmem.sv
@@ -5,36 +5,30 @@
 // 
 //     Create Date: 2023-02-07
 //     Module Name: dmem
-//     Description: 32-bit RISC memory ("data" segment)
+//     Description: 16-bit RISC memory ("data" segment)
 //
 // Revision: 1.0
 //
 //////////////////////////////////////////////////////////////////////////////////
 `ifndef DMEM
 `define DMEM
-
 `timescale 1ns/100ps
 
-module dmem
-// n=bit length of register; r=bit length of addr to limit memory and not crash your verilog emulator
-    #(parameter n = 32, parameter r = 6)(
-    //
-    // ---------------- PORT DEFINITIONS ----------------
-    //
-    input  logic           clk, write_enable,
-    input  logic [(n-1):0] addr, writedata,
-    output logic [(n-1):0] readdata
-);
-    //
-    // ---------------- MODULE DESIGN IMPLEMENTATION ----------------
-    //
-    logic [(n-1):0] RAM[0:(2**r-1)];
+module dmem (
+    input logic clk, memWrite,
+    input logic [15:0] address, writeData,
+    output logic [15:0] readData
+    );
 
-    assign readdata = RAM[addr[(n-1):2]]; // word aligned (ignores lower 2 bits of address)
+    logic [15:0] RAM[32767:0]; //2^15-1
 
-    always @(posedge clk) // write on posedge
-        if (write_enable) RAM[addr[(n-1):2]] <= writedata;
+    assign readData = RAM[address[15:1]];
+
+    always @(posedge clk) begin
+        if (memWrite) RAM[address[15:1]] <= writeData;
+end
 
 endmodule
 
-`endif // DMEM
+`endif
+
